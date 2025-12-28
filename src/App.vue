895e0c27b906975ab9eb3990e7691ad2a37a7fb7
Dao_you <qaz00639@gmail.com>
2025-12-28 22:54:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import LinkCard from './components/LinkCard.vue'
+
+const links = [
+  { url: 'https://links.cppc.tw', msg: '測試' },
+  { url: 'https://links.cppc.tw', msg: '測試' },
+]
+</script>
+
+<template>
+  <div class="main-container">
+    <LinkCard url="https://links.cppc.tw"> 測試 </LinkCard>
+    <LinkCard url="https://links.cppc.tw"> 測試 </LinkCard>
+  </div>
+</template>
+
+<style>
+body {
+}
+.main-container {
+  width: 100vw;
+  max-width: 720px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
---
+++ src/components/LinkCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  url: string
+}>()
+</script>
+
+<template>
+  <a class="card" :href="url" target="_blank" rel="noopener noreferrer">
+    <h2><slot></slot></h2>
+    <p>{{ url }}</p>
+  </a>
+</template>
+
+<style scoped>
+.card {
+  text-decoration: none;
+  color: #540b0e;
+  display: block;
+  padding: 1rem 1.5rem;
+  margin: 1.5rem;
+  border-radius: 12px;
+  text-align: left;
+  transition: box-shadow 0.2s ease-in-out;
+  border: 1px solid #e0a899;
+  box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.05);
+
+  h2 {
+    margin: 0;
+    /* font-size: 1.5rem; */
+  }
+
+  p {
+    margin: 0;
+  }
+}
+
+.card:hover {
+  box-shadow: 0 20px 30px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
